f7bf384d4bc873fc6f5ea3a9cb9a34113e132767
Gavin Isgar <gisgar3@gmail.com>
2019-10-02 12:57:20 -0400

    Updated file-catching method to continue after finding non-Java files.

--- 2019-2020/UniversalLoader/loader.java
+++ 2019-2020/UniversalLoader/loader.java
@@ -14,21 +14,26 @@ import java.nio.file.*;
 public class loader {
 
     public static JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+    public static ArrayList<String> fileTokens = new ArrayList<String>();
 
     public static void main(String[] args) {
-        System.out.println("Input a valid file-system path to compile Java class.");
+        System.out.println("Input a valid file-system path to compile a Java source file.");
         String data = System.console().readLine();
         File filepath = new File(data);
         if (filepath.exists()) {
-            System.out.println("\nFile-system path exists; beginning checks for valid Java classes.");
+            System.out.println("\nFile-system path exists; beginning checks for valid Java source files.");
             try {
                 // Make thread sleep for 1500 milliseconds; give user time to read output
                 Thread.sleep(1500);
-                for (File file: filepath.listFiles()) {
+                for (File file : filepath.listFiles()) {
                     if (file.getName().endsWith(".java")) {
+                        fileTokens.add(file.getName());
                         System.out.println("\n" + file.getName());
                         compiler.run(null, null, null, filepath + "\\" + file.getName());
                         System.out.format("%s has been compiled successfully.\n", file.getName());
+                    }
+                    else if (file.getName().endsWith(".java") == false) {
+                        continue;
                     }
                 }
             }
